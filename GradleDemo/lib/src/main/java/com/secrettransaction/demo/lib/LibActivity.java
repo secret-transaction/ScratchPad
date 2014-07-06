4ba6805fa1f2c4799276d7edef716419fce9b189
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-07-06 12:33:27 +0800

    Added Sample Code for Adding a Library Module to the App Module

--- GradleDemo/lib/src/main/java/com/secrettransaction/demo/lib/LibActivity.java
+++ GradleDemo/lib/src/main/java/com/secrettransaction/demo/lib/LibActivity.java
@@ -1,32 +1,24 @@
-package com.secrettransaction.gradledemo;
+package com.secrettransaction.demo.lib;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-
-import com.secrettransaction.demo.lib.Wazzuper;
 
 
-public class MainActivity extends Activity {
-
-    TextView helloWorld;
+public class LibActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        helloWorld = (TextView) findViewById(R.id.helloWorld);
-        helloWorld.setText(Wazzuper.wazzup());
+        setContentView(R.layout.activity_lib);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.lib, menu);
         return true;
     }
 
---
+++ GradleDemo/lib/src/main/java/com/secrettransaction/demo/lib/Wazzuper.java
@@ -0,0 +1,11 @@
+package com.secrettransaction.demo.lib;
+
+/**
+ * Created by lbibera on 7/6/14.
+ */
+public class Wazzuper {
+
+    public static String wazzup() {
+        return "Wazzup!";
+    }
+}
