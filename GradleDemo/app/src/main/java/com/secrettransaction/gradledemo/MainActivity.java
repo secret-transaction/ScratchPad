3841b8ff088faea81b724c238eb16a38c9fe3993
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-07-06 13:09:11 +0800

    Added Sample Code for Using an Android Client with Google Cloud Endpoint

--- GradleDemo/app/src/main/java/com/secrettransaction/gradledemo/MainActivity.java
+++ GradleDemo/app/src/main/java/com/secrettransaction/gradledemo/MainActivity.java
@@ -1,18 +1,31 @@
 package com.secrettransaction.gradledemo;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.appspot.hello_trash.helloworld.Helloworld;
+import com.appspot.hello_trash.helloworld.model.HelloGreeting;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
 import com.secrettransaction.demo.lib.Wazzuper;
+
+import java.io.IOException;
 
 
 public class MainActivity extends Activity {
 
     TextView helloWorld;
+    Helloworld api;
+    Button apiButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +35,49 @@ public class MainActivity extends Activity {
         helloWorld = (TextView) findViewById(R.id.helloWorld);
         helloWorld.setText(Wazzuper.wazzup());
 
-        Helloworld.Builder builder = null;
+        JsonFactory JSON_FACTORY = new AndroidJsonFactory();
+        HttpTransport HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
+        Helloworld.Builder builder = new Helloworld.Builder(HTTP_TRANSPORT, JSON_FACTORY, null);
+        api = builder.build();
+
+        apiButton = (Button) findViewById(R.id.apiButton);
+        apiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onApiButtonClick();
+            }
+        });
     }
 
+    public void onApiButtonClick() {
+
+        AsyncTask<Void, Void, HelloGreeting> sendGreetings = new AsyncTask<Void, Void, HelloGreeting> () {
+            @Override
+            protected HelloGreeting doInBackground(Void... unused) {
+                // Retrieve service handle.
+                Helloworld apiServiceHandle = api;
+
+                try {
+                    HelloGreeting greeting = new HelloGreeting();
+                    greeting.setMessage("test message");
+
+                    Helloworld.Greetings.Multiply multiplyGreetingCommand = apiServiceHandle.greetings().multiply(10, greeting);
+                    greeting = multiplyGreetingCommand.execute();
+                    return greeting;
+                } catch (IOException e) {
+                    Log.e("API", "Exception during API call", e);
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(HelloGreeting greeting) {
+
+            }
+        };
+
+        sendGreetings.execute((Void)null);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
