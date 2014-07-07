479d6791e5d074701701f047e219ef5edbd158e6
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-07-08 00:22:23 +0800

    Updated code with xcode-generated template for Core Data

--- DatabasePractice/DatabasePractice/STDViewController.m
+++ DatabasePractice/DatabasePractice/STDViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "STDViewController.h"
+#import "STDAppDelegate.h"
 
 @interface STDViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *name;
+@property (weak, nonatomic) IBOutlet UITextField *address;
 
 @end
 
@@ -24,6 +27,22 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)save
+{
+    NSLog(@"Save:%@,%@", self.name.text, self.address.text);
+    
+    STDAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    
+    NSManagedObjectContext *context = appDelegate.managedObjectContext;
+    
+    NSManagedObject *person = [NSEntityDescription insertNewObjectForEntityForName:@"Person" inManagedObjectContext:context];
+    [person setValue:self.name.text forKey:@"name"];
+    [person setValue:self.address.text forKey:@"address"];
+    
+    NSError *error;
+    [context save:&error];
 }
 
 @end
