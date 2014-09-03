ce1590eae97bf7e84f3e73841724910708531639
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-09-03 14:17:29 +0800

    added card model

---
+++ HegartyLectures/Card Game/Card Game/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Card Game
+//
+//  Created by Lyndon Michael Bibera on 9/3/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ HegartyLectures/Card Game/Card Game/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Card Game
+//
+//  Created by Lyndon Michael Bibera on 9/3/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
