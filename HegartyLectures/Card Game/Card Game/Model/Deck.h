1159caa5336018dafb4eb813feb6d5f4619c43b6
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-09-03 15:02:27 +0800

    added Deck model

---
+++ HegartyLectures/Card Game/Card Game/Model/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  Card Game
+//
+//  Created by Lyndon Michael Bibera on 9/3/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+@import Foundation;
+
+@interface Deck : NSObject
+
+@end
---
+++ HegartyLectures/Card Game/Card Game/Model/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  Card Game
+//
+//  Created by Lyndon Michael Bibera on 9/3/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
