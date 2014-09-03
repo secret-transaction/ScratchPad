c76e19bb9db7a7070414dad815d2e7141e3e8b61
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-09-03 14:57:27 +0800

    finished lecture 1 and added new stuffs to the card model

--- HegartyLectures/Card Game/Card Game/Model/Card.h
+++ HegartyLectures/Card Game/Card Game/Model/Card.h
@@ -6,8 +6,27 @@
 //  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+//iOS7 module import
+@import Foundation;
 
 @interface Card : NSObject
+
+#pragma mark - Properties
+
+//strong - ARC stuff, as long as a strong pointer exists for this object, it wont be deallocated
+//nonatomic - dont guarantee thread safety, keep it simple
+//@property - free getters/setters for _contents
+//name of the getter = contents, and setter = setContents
+@property (strong, nonatomic) NSString *contents;
+
+//primitive = hence no strong/weak (doesnt live in the heap)
+@property (nonatomic, getter = isChosen) BOOL chosen;
+@property (nonatomic, getter = isMatched) BOOL matched;
+
+#pragma mark - Methods
+
+//matches a card against another card
+//good match will return a higher score
+- (int)match:(NSArray *)otherCards;
 
 @end
--- HegartyLectures/Card Game/Card Game/Model/Card.m
+++ HegartyLectures/Card Game/Card Game/Model/Card.m
@@ -8,6 +8,44 @@
 
 #import "Card.h"
 
+//private properties here (ussually)
+@interface Card()
+
+@end
+
 @implementation Card
+
+@synthesize contents = _contents;
+
+//this is how the free getter looks like
+- (NSString *)contents
+{
+    return _contents;
+}
+
+//this is how the free setter looks like
+- (void)setContents:(NSString *)contents
+{
+    _contents = contents;
+}
+
+- (int)match:(NSArray *)otherCards
+{
+    //no need to initialize with zero
+    //but Paul Hegarty believes that this
+    //is a cool thing to do
+    int score = 0;
+    
+    for (Card *card in otherCards) {
+        //simple scoring using content matching
+        //1 point for a match
+        if ([card.contents isEqualToString:self.contents]) {
+            score = 1;
+            break;
+        }
+    }
+    
+    return score;
+}
 
 @end
