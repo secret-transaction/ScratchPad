39ed45000edecf0109311c4e431c2e542683c1ec
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-09-03 14:07:58 +0800

    initial cardgame

---
+++ HegartyLectures/Card Game/Card Game/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Card Game
+//
+//  Created by Lyndon Michael Bibera on 9/3/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HegartyLectures/Card Game/Card Game/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Card Game
+//
+//  Created by Lyndon Michael Bibera on 9/3/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
