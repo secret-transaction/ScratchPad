438f35a170e539b7a7eef01dd31059e626e3dc2b
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-09-05 10:24:21 +0800

    added CardMatchingGame

---
+++ HegartyLectures/Card Game/Card Game/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Card Game
+//
+//  Created by Lyndon Michael Bibera on 9/5/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
--- HegartyLectures/Card Game/Card Game/ViewController.m
+++ HegartyLectures/Card Game/Card Game/ViewController.m
@@ -10,12 +10,23 @@
 
 @interface ViewController ()
 
+//views are held by the ViewController using a weak
+//pointer, because the view is held strongly by the root view
 @property (weak, nonatomic) IBOutlet UIButton *card;
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+
 @property (nonatomic, getter = isBack) BOOL back;
+@property (nonatomic) int flipCount;
 
 @end
 
 @implementation ViewController
+
+- (void)setFlipCount:(int)flipCount
+{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %i", _flipCount];
+}
 
 - (void)viewDidLoad
 {
@@ -34,6 +45,7 @@
     [sender setBackgroundImage:[UIImage imageNamed:backgroundImage] forState:UIControlStateNormal];
     
     self.back = !self.isBack;
+    self.flipCount++;
 }
 
 @end
