806056f325ecccb9d4919cb576027e194085d4ab
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-09-13 17:56:05 +0800

    added Card

---
+++ HegartyLectures/Matchismo/Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Lyndon Michael Bibera on 9/13/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ HegartyLectures/Matchismo/Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Lyndon Michael Bibera on 9/13/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
