c040f6e1ded2d0962f763e2c16ece8b887e50b42
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-09-15 16:31:54 +0800

    added life cycle eperiments

--- HegartyLectures/VC Life Cycle/VC Life Cycle/ViewController.m
+++ HegartyLectures/VC Life Cycle/VC Life Cycle/ViewController.m
@@ -9,19 +9,71 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *testLabel;
+@property (weak, nonatomic) IBOutlet UIButton *testButton;
+@property (weak, nonatomic) IBOutlet UITextView *testBody;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    [self displayContents:@"awakeFromNib"];
 }
 
-- (void)didReceiveMemoryWarning {
+- (instancetype)initWithNibName:(NSString *)name bundle:(NSBundle *)bundle
+{
+    self = [super initWithNibName:name bundle:bundle];
+    
+    if (self) {
+        [self displayContents:@"initWithNibName"];
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    //Outlets loaded
+    [super viewDidLoad];
+    [self displayContents:@"viewDidLoad"];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [self displayContents:@"viewDidLayoutSubviews"];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self displayContents:@"viewDidAppear"];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [self displayContents:@"viewWillDisappear"];
+}
+
+- (IBAction)touchButton:(id)sender
+{
+    [self displayContents:@"touchButton"];
+}
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self displayContents:@"didReceiveMemoryWarning"];
+}
+
+- (void)displayContents:(NSString *)source
+{
+    NSLog(@"======================================");
+    NSLog(@"Displaying from %@", source);
+    NSLog(@"Label: %@", self.testLabel);
+    NSLog(@"Button: %@", self.testButton);
+    NSLog(@"Body: %@", self.testButton);
 }
 
 @end
