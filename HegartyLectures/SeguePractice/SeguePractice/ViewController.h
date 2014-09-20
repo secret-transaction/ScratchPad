489fd5c15ca0f20da2252b2ed975ceefb2dc79e2
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-09-20 15:09:58 +0800

    added segue practice app

---
+++ HegartyLectures/SeguePractice/SeguePractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SeguePractice
+//
+//  Created by Lyndon Michael Bibera on 9/20/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
