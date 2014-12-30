fba54ad75319a1599d1d10c2e5ffd52424dec053
Lyndon Michael Bibera <secret.transaction@gmail.com>
2014-12-30 14:03:23 +0800

    added new project to play around with table and collection views

---
+++ TableAndCollectionView/TableAndCollectionView/ViewControllers/SampleTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SampleTableViewController.h
+//  TableAndCollectionView
+//
+//  Created by Lyndon Michael Bibera on 12/30/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleTableViewController : UITableViewController
+
+@end
---
+++ TableAndCollectionView/TableAndCollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableAndCollectionView
+//
+//  Created by Lyndon Michael Bibera on 12/30/14.
+//  Copyright (c) 2014 Secret Transaction Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
