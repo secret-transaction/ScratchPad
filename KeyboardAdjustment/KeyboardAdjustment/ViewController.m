6c7f0188afbe19b39013c94dddbfd96840b6171e
Lyndon Michael Bibera <secret.transaction@gmail.com>
2015-01-18 22:47:39 +0800

    added keyboard adjustment codes

--- KeyboardAdjustment/KeyboardAdjustment/ViewController.m
+++ KeyboardAdjustment/KeyboardAdjustment/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *layoutConstraint;
 
 @end
 
@@ -16,12 +18,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWasShown:) name:UIKeyboardDidShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillBeHidden:) name:UIKeyboardWillHideNotification object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)keyboardWasShown:(NSNotification*)notification {
+    NSLog(@"Keyboard Was Shown");
+    
+    NSDictionary *userInfo = notification.userInfo;
+    NSTimeInterval duration = [userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    UIViewAnimationCurve curve = [userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue];
+    
+    CGRect keyboardFrameEnd = [userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    keyboardFrameEnd = [self.view convertRect:keyboardFrameEnd fromView:nil];
+    
+    [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionBeginFromCurrentState | curve animations:^{
+        //self.containerView.frame = CGRectMake(0, 0, keyboardFrameEnd.size.width, keyboardFrameEnd.origin.y);
+        self.layoutConstraint.constant = keyboardFrameEnd.size.height;
+        [self.view layoutIfNeeded];
+    } completion:nil];
+    
+}
+
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification {
+    NSLog(@"Keyboard Will Be Hidden");
 }
 
 @end
